99f37d4ca7b13e7d8255cabd687560edbf913bc3
Thomas Jensen <thomas.jensen_@outlook.com>
2025-01-11 15:23:41 +0100

    Trying to add Chicago95

--- hosts/msi-prestige/default.nix
+++ hosts/msi-prestige/default.nix
@@ -177,6 +177,17 @@
     dedicatedServer.openFirewall = true;
   };
 
+  nixpkgs.overlays = [
+    (self: super: {
+      chicago95-theme = super.fetchFromGitHub {
+        owner = "grassmunk";
+        repo = "Chicago95";
+        rev = "master";
+        sha256 = "1jmv6cxvsbfqsdg12hdpjivglpqw74bwv31aig5a813cfz58g49b";
+      };
+    })
+  ];
+
   # System packages
   environment.systemPackages = with pkgs; [
     # Basic utilities
@@ -252,12 +263,7 @@
     xfce.xfce4-settings
     sound-theme-freedesktop
 
-    (pkgs.callPackage (fetchFromGitHub {
-        owner = "grassmunk";
-        repo = "Chicago95";
-        rev = "master";
-        sha256 = "1jmv6cxvsbfqsdg12hdpjivglpqw74bwv31aig5a813cfz58g49b";
-      }) {})
+    chicago95-theme
   ];
 
   # Fonts
@@ -272,8 +278,8 @@
     system.activationScripts.installChicago95 = ''
       mkdir -p /home/thomas/.themes
       mkdir -p /home/thomas/.icons
-      cp -r ${pkgs.Chicago95}/Theme/Chicago95 /home/thomas/.themes/
-      cp -r ${pkgs.Chicago95}/Icons/Chicago95 /home/thomas/.icons/
+      cp -r ${pkgs.chicago95-theme}/Theme/Chicago95 /home/thomas/.themes/
+      cp -r ${pkgs.chicago95-theme}/Icons/Chicago95 /home/thomas/.icons/
       chown -R thomas:users /home/thomas/.themes
       chown -R thomas:users /home/thomas/.icons
     '';
