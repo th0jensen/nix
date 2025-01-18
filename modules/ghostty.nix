eeb6ec108eae6d767f505a48e24e8e7ea091797e
Thomas Jensen <thomas.jensen_@outlook.com>
2025-01-18 17:27:01 +0100

    Fixed Ghostty config

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -1,41 +1,38 @@
 { pkgs, ... }: {
-  home.file.".config/ghostty/config".text = ''
-    confirm-close-surface = false
-    quit-after-last-window-closed = true
-    shell-integration = fish
-    clipboard-read = allow
-    clipboard-write = allow
-    window-vsync = true
-    window-colorspace = display-p3
-    window-padding-x = 0
-    window-padding-y = 0
-    theme = gruber-darker
-    selection-invert-fg-bg = true
-    cursor-style-blink = false
-    font-thicken = true
-    macos-non-native-fullscreen = true
-    macos-option-as-alt = left
-    macos-icon = custom-style
-    macos-icon-frame = beige
-    macos-icon-ghost-color = ffdd33
-    macos-icon-screen-color = 191919
-    gtk-adwaita = false
+  programs.ghostty = {
+    enable = true;
+    package = if pkgs.stdenv.isDarwin then
+    pkgs.runCommand "ghostty-dummy" {
+      meta.mainProgram = "ghostty";
+    } ''
+      mkdir -p $out/bin
+      echo "#!/bin/sh" > $out/bin/ghostty
+      echo "exec /Applications/Ghostty.app/Contents/MacOS/ghostty \"\$@\"" >> $out/bin/ghostty
+      chmod +x $out/bin/ghostty
+    ''
+    else pkgs.ghostty;
+    enableFishIntegration = true;
 
-    # custom-shader = shaders/animated-gradient-shader.glsl
-    # custom-shader = shaders/bettercrt.glsl
-    # custom-shader = shaders/bloom.glsl
-    # custom-shader = shaders/crt.glsl
-    # custom-shader = shaders/cubes.glsl
-    # custom-shader = shaders/dither.glsl
-    # custom-shader = shaders/drunkard.glsl
-    # custom-shader = shaders/glitchy.glsl
-    # custom-shader = shaders/gradient-background.glsl
-    # custom-shader = shaders/just-snow.glsl
-    # custom-shader = shaders/negative.glsl
-    # custom-shader = shaders/retro-terminal.glsl
-    # custom-shader = shaders/smoke-and-ghost.glsl
-    # custom-shader = shaders/starfield-colors.glsl
-    # custom-shader = shaders/starfield.glsl
-    # custom-shader = shaders/tft.glsl
-  '';
+    settings = {
+      confirm-close-surface = false;
+      quit-after-last-window-closed = true;
+      shell-integration = "fish";
+      clipboard-read = "allow";
+      clipboard-write = "allow";
+      window-vsync = true;
+      window-colorspace = "display-p3";
+      window-padding-x = 0;
+      window-padding-y = 0;
+      theme = "gruber-darker";
+      selection-invert-fg-bg = true;
+      cursor-style-blink = false;
+      font-thicken = true;
+      macos-non-native-fullscreen = true;
+      macos-option-as-alt = "left";
+      macos-icon = "custom-style";
+      macos-icon-frame = "beige";
+      macos-icon-ghost-color = "ffdd33";
+      macos-icon-screen-color = "191919";
+    };
+  };
 }
