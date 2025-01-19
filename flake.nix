14cc0cf8a45a11514aa92b45377369ba9f50d14c
Thomas Jensen <thomas.jensen_@outlook.com>
2025-01-19 06:02:49 +0100

    Home Manager failed :(

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.thomas = import ./users/thomas/nixos.nix;
           }
