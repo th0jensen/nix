88c4f98c1d88eec07b2746269da86f55cdf85b75
Thomas Jensen <thomas.jensen_@outlook.com>
2025-05-29 16:30:23 +0200

    Fix

--- hosts/msi-prestige/desktop.nix
+++ hosts/msi-prestige/desktop.nix
@@ -181,18 +181,4 @@
     style = "gtk2";
   };
 
-  # Enable automatic screen locking
-  programs.swaylock = {
-    package = pkgs.swaylock-effects;
-    settings = {
-      color = "000000";
-      font-size = 24;
-      indicator-idle-visible = false;
-      indicator-radius = 100;
-      line-color = "ffffff";
-      show-failed-attempts = true;
-    };
-  };
-
-
 }
